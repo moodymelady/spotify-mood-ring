8edb5901c22d540f48d81d6d2b7f463f53126a76
moodymelady <152474570+moodymelady@users.noreply.github.com>
2025-08-27 18:36:06 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+from utils import get_spotify_client
+
+def main():
+    sp = get_spotify_client()
+    user = sp.current_user()
+    print("Logged in as:", user["display_name"])
+
+if __name__ == "__main__":
+    main()
 
