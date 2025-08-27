7c478e5cf306ade90055d48417b70398e7fe5aef
moodymelady <152474570+moodymelady@users.noreply.github.com>
2025-08-27 18:31:55 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,23 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
 
+def get_spotify_client():
+    # load .env vars
+    load_dotenv()
+
+    client_id = os.getenv("SPOTIPY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+    redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+
+    scope = "user-read-private"  # basic scope just to test
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        scope=scope
+    ))
+
+    return sp
